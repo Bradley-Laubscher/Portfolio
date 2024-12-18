2742e26d5f5134985b511be2084ffe88ee0d0356
Bradley-Laubscher <bradleylaubscher@gmail.com>
2024-12-18 09:32:30 +0200

    Corrected Info tooltip color on Project page. Set static project name color.

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -10,7 +10,7 @@ function Projects() {
             <section className='projects-container'>
                 <div className='projects sa-surf-spots'>
                     <div className='tooltip'>
-                        <img src='../Resources/info.png' alt='more information icon' className='infoIcon'></img>
+                        <img src='../Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A webapp build to display marked popular surf spots on a map of South Africa<br/>Built using:<br/> AngularJS, JavaScript, Html and CSS</span>
                     </div>
                     <p className='project-name'>SA Surf Spots</p>
@@ -18,7 +18,7 @@ function Projects() {
                 </div>
                 <div className='projects duly-noted'>
                     <div className='tooltip'>
-                        <img src='../Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
+                        <img src='../Resources/info.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A notepad webapp which allows the user to take notes, create categories for notes and remove/update/filter notes.<br/>Built using:<br/> JavaScript, React, Redux, Html and CSS</span>
                     </div>
                     <p className='project-name'>Duly Noted</p>
@@ -26,7 +26,7 @@ function Projects() {
                 </div>
                 <div className='projects reddit-app'>
                     <div className='tooltip'>
-                        <img src='../Resources/info.png' alt='more information icon' className='infoIcon'></img>
+                        <img src='../Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A webapp used to search Reddits database and display articles which match the search.<br/>Built using:<br/> JavaScript, React, Html and CSS</span>
                     </div>                   
                     <p className='project-name'>Reddit Minimal</p>
@@ -42,7 +42,7 @@ function Projects() {
                 </div>
                 <div className='projects tea-cozy'>
                     <div className='tooltip'>
-                        <img src='../Resources/info.png' alt='more information icon' className='infoIcon'></img>
+                        <img src='../Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A homepage for a tea store advertising their products, location and contact info.<br/>Built using:<br/> Html and CSS</span>
                     </div>
                     <p className='project-name'>Tea Cozy</p>
@@ -50,7 +50,7 @@ function Projects() {
                 </div>
                 <div className='projects surf-club'>
                     <div className='tooltip'>
-                        <img src='../Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
+                        <img src='../Resources/info.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A homepage for a surf club advertising their purpose and displaying a few images to entice surfers to join.<br/>Built using:<br/> Html and CSS</span>
                     </div>
                     <p className='project-name'>Surf Club</p>
