5d410f5e91038e7cc141e8a786c02901b032ab8c
Bradley-Laubscher <bradleylaubscher@gmail.com>
2025-01-19 14:06:52 +0200

    Moved target url to jsx

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -8,7 +8,7 @@ function Projects() {
         animate={{ opacity: 1, transition: {type: "fade", duration: 1}}}
         exit={{ x: window.innerWidth, opacity: 0, transition: {type: "fade", duration: 1}}}>
             <section className='projects-container'>
-                <div className='projects sa-surf-spots'>
+                <div className='projects sa-surf-spots' style={{backgroundImage: 'url(/Resources/map-of-south-africa.gif)'}}>
                     <div className='tooltip'>
                         <img src='/Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A webapp build to display marked popular surf spots on a map of South Africa<br/>Built using:<br/> AngularJS, JavaScript, Html and CSS</span>
@@ -16,7 +16,7 @@ function Projects() {
                     <p className='project-name'>SA Surf Spots</p>
                     <button><a href='https://sa-surf-spots.vercel.app/' target='_blank' rel="noreferrer">Visit Site</a></button>
                 </div>
-                <div className='projects duly-noted'>
+                <div className='projects duly-noted' style={{backgroundImage: 'url(/Resources/NotePad.jpeg)'}}>
                     <div className='tooltip'>
                         <img src='/Resources/info.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A notepad webapp which allows the user to take notes, create categories for notes and remove/update/filter notes.<br/>Built using:<br/> JavaScript, React, Redux, Html and CSS</span>
@@ -24,7 +24,7 @@ function Projects() {
                     <p className='project-name'>Duly Noted</p>
                     <button><a href='https://dulynoted-webapp.netlify.app/' target='_blank' rel='noreferrer' >Visit Site</a></button>
                 </div>
-                <div className='projects reddit-app'>
+                <div className='projects reddit-app' style={{backgroundImage: 'url(/Resources/RedditLogo.jpg)'}}>
                     <div className='tooltip'>
                         <img src='/Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A webapp used to search Reddits database and display articles which match the search.<br/>Built using:<br/> JavaScript, React, Html and CSS</span>
@@ -32,7 +32,7 @@ function Projects() {
                     <p className='project-name'>Reddit Minimal</p>
                     <button><a href='https://redditminimalproject.netlify.app/' target='_blank' rel='noreferrer' >Visit Site</a></button>
                 </div>
-                <div className='projects top-travel-destinations'>
+                <div className='projects top-travel-destinations' style={{backgroundImage: 'url(/Resources/Travel.jpg)'}}>
                     <div className='tooltip'>
                         <img src='/Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A website providing information on top travel destination around the world and a few tips for traveling.<br/>Built using:<br/> Next.js, React, Javascript, JSX and CSS</span>
@@ -40,7 +40,7 @@ function Projects() {
                     <p className='project-name'>Top Travel Destinations</p>
                     <button><a href='https://top-travel-destinations.vercel.app/' target='_blank' rel="noreferrer">Visit Site</a></button>
                 </div>
-                <div className='projects tea-cozy'>
+                <div className='projects tea-cozy' style={{backgroundImage: 'url(/Resources/organic\\ tea.jpg)'}}>
                     <div className='tooltip'>
                         <img src='/Resources/info-2.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A homepage for a tea store advertising their products, location and contact info.<br/>Built using:<br/> Html and CSS</span>
@@ -48,7 +48,7 @@ function Projects() {
                     <p className='project-name'>Tea Cozy</p>
                     <button><a href='https://bradley-laubscher.github.io/Tea-Cozy/' target='_blank' rel="noreferrer">Visit Site</a></button>
                 </div>
-                <div className='projects surf-club'>
+                <div className='projects surf-club' style={{backgroundImage: 'url(/Resources/Surf-boards.jpg)'}}>
                     <div className='tooltip'>
                         <img src='/Resources/info.png' alt='more information icon' className='infoIcon'></img>
                         <span className='tooltiptext'>A homepage for a surf club advertising their purpose and displaying a few images to entice surfers to join.<br/>Built using:<br/> Html and CSS</span>
