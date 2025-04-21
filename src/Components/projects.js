4ab75e8c6403e6e9f9fbf7bbbcb0b9f927065c02
Bradley-Laubscher <bradleylaubscher@gmail.com>
2025-04-21 19:43:54 +0200

    Added NextUp project, removed Surf club homepage

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -15,6 +15,43 @@ function Projects() {
       <section className="projects-container">
         
         <a
+          href="https://next-up-8qce.onrender.com"
+          target="_blank"
+          rel="noreferrer"
+          className="projects"
+          style={{ backgroundImage: 'url(/Resources/NextUp.png)' }}
+        >
+          <div className="tooltip">
+            <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
+            <span className="tooltiptext">
+              A game tracking tool with notifications regarding updates and sales. Makes use of the IGDB(Twitch) and Steam API for the gathering of game information.<br /><br />Built using:<br /> .NET/c#
+            </span>
+          </div>
+          <p className="project-name">NextUp</p>
+        </a>
+
+        <a
+          href="https://surfspot.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="projects"
+          style={{
+            backgroundImage: 'url(/Resources/map-of-south-africa.gif)',
+            backgroundSize: 'cover', // or 'contain' depending on your design
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          <div className="tooltip">
+            <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
+            <span className="tooltiptext">
+              An application displaying surf conditions for some of Cape Town's local spots. Making use of the Open-Meteo marine API.<br /><br />Built using:<br /> Dart and Flutter
+            </span>
+          </div>
+          <p className="project-name">Surf Spot</p>
+        </a>
+
+        <a
           href="https://bradley-laubscher.github.io/marketplace/"
           target="_blank"
           rel="noreferrer"
@@ -24,26 +61,10 @@ function Projects() {
           <div className="tooltip">
             <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
-              A dynamic marketplace for multiple merchant types.<br />Built using:<br /> Dart and Flutter
+              A dynamic marketplace for multiple merchant types.<br /><br />Built using:<br /> Dart and Flutter
             </span>
           </div>
           <p className="project-name">Marketplace</p>
-        </a>
-
-        <a
-          href="https://surfspot.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-          className="projects"
-          style={{ backgroundImage: 'url(/Resources/map-of-south-africa.gif)' }}
-        >
-          <div className="tooltip">
-            <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
-            <span className="tooltiptext">
-              An application displaying surf conditions for some of Cape Town's local spots. Making use of the Open-Meteo marine API.<br />Built using:<br /> Dart and Flutter
-            </span>
-          </div>
-          <p className="project-name">Surf Spot</p>
         </a>
 
         <a
@@ -56,7 +77,7 @@ function Projects() {
           <div className="tooltip">
             <img src="/Resources/info.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
-              A notepad webapp that allows the user to take notes, create categories for notes, and remove/update/filter notes.<br />Built using:<br /> JavaScript, React, Redux, Html, and CSS
+              A notepad webapp that allows the user to take notes, create categories for notes, and remove/update/filter notes.<br /><br />Built using:<br /> JavaScript, React, Redux, Html, and CSS
             </span>
           </div>
           <p className="project-name">Duly Noted</p>
@@ -72,7 +93,7 @@ function Projects() {
           <div className="tooltip">
             <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
-              A webapp used to search Reddit's database and display articles that match the search.<br />Built using:<br /> JavaScript, React, Html, and CSS
+              A webapp used to search Reddit's database and display articles that match the search.<br /><br />Built using:<br /> JavaScript, React, Html, and CSS
             </span>
           </div>
           <p className="project-name">Reddit Minimal</p>
@@ -88,26 +109,10 @@ function Projects() {
           <div className="tooltip">
             <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
-              A website providing information on top travel destinations around the world and a few tips for traveling.<br />Built using:<br /> Next.js, React, Javascript, JSX, and CSS
+              A website providing information on top travel destinations around the world and a few tips for traveling.<br /><br />Built using:<br /> Next.js, React, Javascript, JSX, and CSS
             </span>
           </div>
           <p className="project-name">Top Travel Destinations</p>
-        </a>
-
-        <a
-          href="https://bradley-laubscher.github.io/Surf-Club/"
-          target="_blank"
-          rel="noreferrer"
-          className="projects"
-          style={{ backgroundImage: 'url(/Resources/Surf-boards.jpg)' }}
-        >
-          <div className="tooltip">
-            <img src="/Resources/info.png" alt="more information icon" className="infoIcon" />
-            <span className="tooltiptext">
-              A homepage for a surf club advertising their purpose and displaying a few images to entice surfers to join.<br />Built using:<br /> Html and CSS
-            </span>
-          </div>
-          <p className="project-name">Surf Club</p>
         </a>
 
       </section>
