d51d0c1fd1e345fcf6074d2c3559988b536f28f4
Bradley-Laubscher <bradleylaubscher@gmail.com>
2025-04-22 20:26:25 +0200

    Changed project images

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -22,7 +22,7 @@ function Projects() {
           style={{ backgroundImage: 'url(/Resources/NextUp.png)' }}
         >
           <div className="tooltip">
-            <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
+            <img src="/Resources/info.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
               A game tracking tool with notifications regarding updates and sales. Makes use of the IGDB(Twitch) and Steam API for the gathering of game information.<br /><br />Built using:<br /> .NET/c#
             </span>
@@ -35,12 +35,12 @@ function Projects() {
           target="_blank"
           rel="noreferrer"
           className="projects"
-          style={{ backgroundImage: 'url(/Resources/map-of-south-africa.gif)'}}
+          style={{ backgroundImage: 'url(/Resources/Surf-Boards.jpg)'}}
         >
           <div className="tooltip">
-            <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
+            <img src="/Resources/info.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
-              An application displaying surf conditions for some of Cape Town's local spots. Making use of the Open-Meteo marine API.<br /><br />Built using:<br /> Dart and Flutter
+              An application displaying surf conditions for some of Cape Town's local spots and notifying subscribers when the conditions are good. Making use of the Open-Meteo marine API.<br /><br />Built using:<br /> Dart and Flutter
             </span>
           </div>
           <p className="project-name">Surf Spot</p>
@@ -54,7 +54,7 @@ function Projects() {
           style={{ backgroundImage: 'url(/Resources/Marketplace.jpg)' }}
         >
           <div className="tooltip">
-            <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
+            <img src="/Resources/info.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
               A dynamic marketplace for multiple merchant types.<br /><br />Built using:<br /> Dart and Flutter
             </span>
@@ -86,7 +86,7 @@ function Projects() {
           style={{ backgroundImage: 'url(/Resources/RedditLogo.jpg)' }}
         >
           <div className="tooltip">
-            <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
+            <img src="/Resources/info.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
               A webapp used to search Reddit's database and display articles that match the search.<br /><br />Built using:<br /> JavaScript, React, Html, and CSS
             </span>
@@ -102,7 +102,7 @@ function Projects() {
           style={{ backgroundImage: 'url(/Resources/Travel.jpg)' }}
         >
           <div className="tooltip">
-            <img src="/Resources/info-2.png" alt="more information icon" className="infoIcon" />
+            <img src="/Resources/info.png" alt="more information icon" className="infoIcon" />
             <span className="tooltiptext">
               A website providing information on top travel destinations around the world and a few tips for traveling.<br /><br />Built using:<br /> Next.js, React, Javascript, JSX, and CSS
             </span>
