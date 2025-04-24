6620fc12128372abf3db431b4abd97d06aeece22
Bradley-Laubscher <bradleylaubscher@gmail.com>
2025-04-24 19:57:07 +0200

    Update github link

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -13,7 +13,7 @@ function Contact() {
                     <li>Email: bradleylaubscher@gmail.com</li>
                     <li>Mobile: 0720647765</li>
                     <li><i className="fab fa-linkedin" title='LinkedIn'></i>: <a href='https://www.linkedin.com/in/bradley-laubscher-408711130' target='_blank' rel="noreferrer">Bradley Laubscher</a></li>
-                    <li><i className="fab fa-github-square" title='GitHub'></i>: <a href='https://github.com/Bradley-Laubscher/Portfolio' target='_blank' rel="noreferrer">Bradley-Laubscher</a></li>
+                    <li><i className="fab fa-github-square" title='GitHub'></i>: <a href='https://github.com/Bradley-Laubscher' target='_blank' rel="noreferrer">Bradley-Laubscher</a></li>
                 </ul>
             </section>
         </motion.div>
